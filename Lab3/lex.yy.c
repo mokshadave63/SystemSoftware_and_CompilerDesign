6b29fc700317b72de9fa675ae6d0d945fd3a075c
Moksha Dave <mokshadave63@gmail.com>
2024-08-09 22:15:53 +0530

    Another flex program

--- Lab3/lex.yy.c
+++ Lab3/lex.yy.c
@@ -282,20 +282,21 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[9] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[12] =
     {   0,
-        0,    0,    5,    3,    2,    1,    3,    0
+        0,    0,    6,    4,    4,    4,    2,    1,    3,    3,
+        0
     } ;
 
 static yyconst int yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    2,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +322,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[5] =
     {   0,
-        1,    2,    2
+        1,    2,    1,    3
     } ;
 
-static yyconst short int yy_base[10] =
+static yyconst short int yy_base[14] =
     {   0,
-        0,    0,    5,    0,    6,    6,    0,    6,    3
+        0,    0,   10,   11,    0,    2,   11,   11,    0,    0,
+       11,    5,    6
     } ;
 
-static yyconst short int yy_def[10] =
+static yyconst short int yy_def[14] =
     {   0,
-        8,    1,    8,    9,    8,    8,    9,    0,    8
+       11,    1,   11,   11,   12,   11,   11,   11,   13,   13,
+        0,   11,   11
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[16] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    4,    5,    6,    8,    9,   10,    7,   10,   11,
+        3,   11,   11,   11,   11
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[16] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,    6,    6,   13,   12,   13,    3,
+       11,   11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,12 +362,15 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "lab3.l"
+#line 1 "2.l"
 #define INITIAL 0
-#line 2 "lab3.l"
-    #include<stdio.h>
-    int sc=0, wc=0, lc=0, cc=0;
-#line 366 "lex.yy.c"
+#line 2 "2.l"
+#include<stdio.h>
+int nc = 0;
+FILE *yyout_comment;
+FILE *yyout_code;
+int in_comment = 0;
+#line 374 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -513,9 +521,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 5 "lab3.l"
+#line 8 "2.l"
 
-#line 519 "lex.yy.c"
+#line 527 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -566,13 +574,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 11 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -600,25 +608,48 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "lab3.l"
-{ lc++; cc+=yyleng; }
+#line 9 "2.l"
+{
+in_comment = 1;
+fprintf(yyout_comment, "%s", yytext);
+};
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "lab3.l"
-{ sc++; cc+=yyleng; }
+#line 13 "2.l"
+{
+if (in_comment) {
+in_comment = 0;
+fprintf(yyout_comment, "%s\n", yytext);
+}
+nc++;
+};
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "lab3.l"
-{ wc++; cc+=yyleng; }
+#line 20 "2.l"
+{
+if (!in_comment)
+fprintf(yyout_comment, "%s\n", yytext);
+nc++;
+};
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 9 "lab3.l"
+#line 25 "2.l"
+{
+if (!in_comment)
+fprintf(yyout_code, "%s", yytext);
+else
+fprintf(yyout_comment, "%s", yytext); // Write comment lines
+} // Write non-comment code and comments within multiline comments
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 31 "2.l"
 ECHO;
 	YY_BREAK
-#line 622 "lex.yy.c"
+#line 653 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -910,7 +941,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -945,11 +976,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 11);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1504,24 +1535,20 @@ int main()
 	return 0;
 	}
 #endif
-#line 9 "lab3.l"
+#line 31 "2.l"
 
-int main(int argc, char* argv[])
+int main()
 {
-    FILE *inputFile = fopen("source.txt", "r");
-    if (inputFile == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-    yyin = inputFile;
-    yylex();
-    fclose(inputFile);
-    printf("The number of lines=%d\n", lc);
-    printf("The number of spaces=%d\n", sc);
-    printf("The number of words=%d\n", wc);
-    printf("The number of characters=%d\n", cc);
-
+yyin = fopen("a_ref.txt", "r");
+yyout_comment = fopen("comment.txt", "w");
+yyout_code = fopen("program.c", "w");
+yylex();
+fclose(yyin);
+fclose(yyout_comment);
+fclose(yyout_code);
+return 0;
 }
-int yywrap(){
-    return 1;
+int yywrap()
+{
+return 1;
 }
